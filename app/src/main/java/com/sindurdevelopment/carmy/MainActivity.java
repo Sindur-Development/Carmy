f2f2e946fc9ad75e84700a622a2cc66080db584d
fregget <fbjuren@gmail.com>
2023-03-12 11:23:36 +0100

    New design home

--- app/src/main/java/com/sindurdevelopment/carmy/MainActivity.java
+++ app/src/main/java/com/sindurdevelopment/carmy/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.net.MalformedURLException;
 
@@ -25,37 +26,20 @@ public class MainActivity extends AppCompatActivity {
                     StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-        Button vehicleList = (Button) findViewById(R.id.vehicleList);
-        vehicleList.setOnClickListener(new View.OnClickListener() {
+
+        TextView tempData = findViewById(R.id.tempData);
+        Button vehicleInformationBTN = (Button) findViewById(R.id.vehicleInformation);
+        vehicleInformationBTN.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 try {
-                    vehicleManager = new VehicleManager();;
+                    vehicleManager = new VehicleManager();
+                    tempData.setText(vehicleManager.getTemperature());
                 } catch (Exception e) {
                     System.out.println("Fel30 (skapa vehicleList): " + e);
                 }
             }
         });
 
-        Button tempButton = (Button) findViewById(R.id.temp);
-        tempButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    System.out.println(vehicleManager.getTemperature());
-                } catch (Exception e) {
-                    System.out.println("Fel45 (temp): " + e);
-                }
-            }
-        });
 
-        Button accessibiltyButton = (Button) findViewById(R.id.accessibility);
-        accessibiltyButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    vehicleManager.engineDiagnostic();
-                } catch (Exception e) {
-                    System.out.println("Fel56 (Vehicle information): " + e);
-                }
-            }
-        });
     }
 }
